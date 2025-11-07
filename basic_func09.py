305f5f432f44fa74d93ea0a6d55e4eb084e26a46
Dbek83developer <dbek.developer@gmail.com>
2025-11-07 22:03:33 +0800

    functions

--- basic_func09.py
+++ basic_func09.py
@@ -1,6 +1,8 @@
-# Task: Create a function called decrease_by_one that decreases its input by 1
+# Task: Create a function called cc that decreases its input by 1
 # Function should:
 # 1. Take one parameter 'a'
 # 2. Subtract 1 from the value of 'a'
 # 3. Return the decreased value
 # Example: if a=5, return 4
+def decrease_by_one(a):
+    return a - 1
